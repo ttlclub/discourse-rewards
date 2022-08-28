b0d1ee31e4511fe04655fb463b584aed8b242174
Alienaaa <alien0417new@gmail.com>
2022-08-28 17:25:14 +0900

    add methods to user extension

--- lib/discourse-rewards/user_extension.rb
+++ lib/discourse-rewards/user_extension.rb
@@ -5,5 +5,14 @@ module DiscourseRewards::UserExtension
     base.has_many :user_points, class_name: 'DiscourseRewards::UserPoint'
     base.has_many :user_rewards, class_name: 'DiscourseRewards::UserReward'
     base.has_many :rewards, class_name: 'DiscourseRewards::Reward'
+    base.has_one :total_point, class_name: 'DiscourseRewards::TotalPoint'
+  end
+
+  def get_total_earned_points
+    self.total_point ? self.total_point.read_attribute(:total_earned_points) : 0
+  end
+  
+  def get_available_points
+    self.total_point ? self.total_point.read_attribute(:available_points) : 0
   end
 end
